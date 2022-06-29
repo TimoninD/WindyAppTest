1533ec19081aadd8362dcbea42848561d315abed
Даниил Тимонин <rand1kch@gmail.com>
2022-06-29 15:26:10 +0300

    Added sum logic

--- app/src/main/java/com/windy/windyapptest/domain/MainInteractor.kt
+++ app/src/main/java/com/windy/windyapptest/domain/MainInteractor.kt
@@ -1,4 +1,29 @@
 package com.windy.windyapptest.domain
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.withContext
+
 class MainInteractor {
+
+    suspend fun getFlowsByCount(count: Int): List<Flow<Int>> {
+        return withContext(Dispatchers.IO) {
+            val flows = mutableListOf<Flow<Int>>()
+            for (index in 0 until count) {
+                val flow = flow {
+                    delay(BASE_DELAY * (index + 1))
+                    emit(index + 1)
+                }.flowOn(Dispatchers.IO)
+                flows.add(flow)
+            }
+            flows
+        }
+    }
+
+    companion object {
+        private const val BASE_DELAY = 100L
+    }
 }
--- app/src/main/java/com/windy/windyapptest/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/windy/windyapptest/presentation/viewmodel/MainViewModel.kt
@@ -2,13 +2,23 @@ package com.windy.windyapptest.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.windy.windyapptest.domain.MainInteractor
 import com.windy.windyapptest.presentation.view.MainState
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
+
+    private val interactor = MainInteractor()
+
+    private var sumFlow: MutableSharedFlow<Int>? = null
+    private var sumJob: Job? = null
 
     private val _state = MutableStateFlow(MainState())
     val state = _state.asStateFlow()
@@ -18,7 +28,39 @@ class MainViewModel : ViewModel() {
     }
 
     fun onStartClicked() {
+        viewModelScope.launch {
+            val count = state.value.textValue.toIntOrNull()
+            count?.let {
+                updateState(sums = listOf())
 
+                val flows = interactor.getFlowsByCount(it)
+
+                sumFlow = MutableSharedFlow()
+                collectFinalSumFlow()
+                collectSumFlows(flows)
+            }
+        }
+    }
+
+    private fun collectFinalSumFlow() {
+        viewModelScope.launch {
+            sumFlow?.collect {
+                val oldSums = state.value.sums
+                val newSum = oldSums.sum() + it
+                updateState(sums = oldSums + newSum)
+            }
+        }
+    }
+
+    private fun collectSumFlows(flows: List<Flow<Int>>) {
+        sumJob?.cancel()
+        sumJob = viewModelScope.launch(Dispatchers.IO) {
+            flows.forEach {
+                it.collect {
+                    sumFlow?.emit(it)
+                }
+            }
+        }
     }
 
     private fun updateState(
